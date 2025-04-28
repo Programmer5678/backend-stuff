439c53554511ce8516048f1b1100b47a11b7687c
Ruz <ruz@example.com>
2025-04-29 00:46:30 +0300

    almost finished i thought now new bug...

--- havad/submit-handler.js
+++ havad/submit-handler.js
@@ -2,9 +2,7 @@
 // שליחת הטופס במייל
 function submitAndEmail(e) {
 
-    alert("siuuu")
-
-    // e.preventDefault()
+    e.preventDefault()
 
     const form = document.getElementById('feedback-form');
     if (!form.checkValidity()) {
@@ -18,19 +16,36 @@ function submitAndEmail(e) {
 
         const element = document.querySelector(`[name="${name}"]`);
         if (element.type === 'checkbox') {
-            script_str += (`document.getElementById("${name}").querySelector('input[value="${val}"]').checked = true;\n`)
+            script_str += (`document.querySelector( 'input[value="${val}"][name="${name}"]' ).checked = true;\n`)
         }
-        else{
+        else {
             script_str += ("document.getElementsByName('" + name + "')[0].value = '" + val + "';\n")
         }
-        console.log(name, val)
+        // console.log(name, val)
     }
 
     htmlFile = htmlStart
         + "\n<" + "script>\n"
         + script_str
-        + "\ndocument.querySelector('fieldset').disabled = true;\n "
-        + "\ndocument.getElementById('send-form').style.display = 'none';\n "
+        + "\ndocument.querySelector('fieldset').disabled = true;\n " //+ No cursor pointer! + No grey input!
+        + "\ndocument.querySelectorAll('button').forEach(button => {button.style.display = 'none';});;\n "
+        //Click if zoomed in to signature(boo)
+        + "\nif (document.getElementById('zoom-out')?.offsetHeight > 0) document.getElementById('zoom-wrapper').click();;\n"
+        
+        
+        //Remove cursor pointer:
+
+        + `\ndocument.querySelectorAll('*').forEach(el => {
+            const computedStyle = window.getComputedStyle(el);
+            if (computedStyle.cursor === 'pointer') {
+                el.style.removeProperty('cursor'); // Remove the 'cursor' property if it's set
+            }
+            });\n`
+
+        + "document.body.classList.toggle('default-backgrounds');"
+        + "document.body.offsetHeight;"
+
+        + "\ndocument.querySelectorAll('*').forEach(el => { if (window.getComputedStyle(el).cursor === 'pointer') el.style.cursor = 'default'; });\n"
         + "\nsetCanvasElFromHiddenInput()\n;"
         + "\ncanvas.style.pointerEvents='none' \n"
         + '\nrestoreSelectionsFromInputs()\n'
