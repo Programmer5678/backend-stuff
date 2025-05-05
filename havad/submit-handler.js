b7e3c23dbb4d1be42d325d445009dcc3bfd50361
Ruz <ruz@example.com>
2025-05-06 00:46:40 +0300

    almost there! adding last input validation...

--- havad/submit-handler.js
+++ havad/submit-handler.js
@@ -1,6 +1,6 @@
 
-//Can just copy this... its just one element
-function genPopUp() {
+//Generate a popu element! used for form validation
+function genPopUp(...textRows) {
     const svgNS = "http://www.w3.org/2000/svg";
 
     viewBox = [0, 0, 170, 105]
@@ -38,26 +38,27 @@ function genPopUp() {
     const text = document.createElementNS(svgNS, "text");
     text.setAttribute("x", "85");
     text.setAttribute("y", "52.5");
-    text.setAttribute("font-size", "25");
+    text.setAttribute("font-size", "5");
     text.setAttribute("fill", "red");
     text.setAttribute("text-anchor", "middle");
     text.setAttribute("dominant-baseline", "middle");
 
-    // Create first <tspan>
-    const tspan1 = document.createElementNS(svgNS, "tspan");
-    tspan1.setAttribute("x", "85");
-    tspan1.setAttribute("dy", "0");
-    tspan1.textContent = "שדה זה הוא";
 
-    // Create second <tspan>
-    const tspan2 = document.createElementNS(svgNS, "tspan");
-    tspan2.setAttribute("x", "85");
-    tspan2.setAttribute("dy", "1.2em");
-    tspan2.textContent = "שדה חובה";
+    //dy is the differnece in ys between tspan inside an svg text. increase by 1.2em each row
+    let dy = 0
+    for (const textRow of textRows) {
+        const tspan = document.createElementNS(svgNS, "tspan");
+        tspan.setAttribute("x", "85");
+        tspan.setAttribute("dy", String(dy) + "em");
+        tspan.textContent = textRow; // add text content
+        text.appendChild(tspan);
+
+        dy+=1.2
+    }
+
 
     // Append <tspan> elements to <text>
-    text.appendChild(tspan1);
-    text.appendChild(tspan2);
+
 
     // // Set the text content
 
@@ -73,15 +74,12 @@ function genPopUp() {
     newElWrapper.style.right = "40%"; // Position it at the bottom-right corner
     newElWrapper.style.width = "240px"; // Width of the SVG element
     newElWrapper.style.height = "150px"; // Height of the SVG element
-    newElWrapper.style.backgroundColor = "lightblue"; // Just to see the background
+    // newElWrapper.style.backgroundColor = "lightblue"; // Just to see the background
     newElWrapper.style.zIndex = "1"
 
     newElWrapper.appendChild(newEl)
 
     // newElWrapperCopy = newElWrapper.cloneNode(true)
-
-   
-
 
     return newElWrapper
 
@@ -89,6 +87,7 @@ function genPopUp() {
 
 //Handle an empty input element. get the input wrapper.
 //  add the popup to it. add event listeners to close it
+//If it is required, return true
 function handleEmptyInput(inputEl) {
 
     const inputWrapper = document.getElementById(inputEl.name + "-wrapper")
@@ -97,7 +96,7 @@ function handleEmptyInput(inputEl) {
     if (inputWrapper) {
 
         //Generate popup
-        const popUpWrapper = genPopUp()
+        const popUpWrapper = genPopUp("שדה זה הוא", "שדה חובה")
 
         //append the popup to the input wrapper so it can be placed relative to the inputWrapper(thus the input)
         inputWrapper.appendChild(popUpWrapper)
@@ -127,39 +126,22 @@ function handleEmptyInput(inputEl) {
                 // e.preventDefault()
             }, true
         )
-    
+
+        return false
 
     }
 
     else {
-        console.log(" does not work...")
+        console.log("input doesnt have wrapper parent - so it is not required!!!")
+        return true
     }
 
 }
 
-// document.addEventListener("input", (e) => {
-//     console.log("e.target" ,e.target)
-//     e.target.addEventListener("input", () => {
-//         console.log("same thing from inside")
-//     })
-// })
-
-// שליחת הטופס במייל
-function submitEventHandler(e) {
-
-
-
-    alert("here")
-
-    e.preventDefault()
-
-    alert("here")
+//Checks if any input that is required(parent has certain parent class - wrapper)
+function handleEmptyInputs() {
 
     const form = document.getElementById('feedback-form');
-    // if (!form.checkValidity()) {
-    //     form.reportValidity();
-    //     return;
-    // }
 
 
     for (const el of form.elements) {
@@ -172,19 +154,20 @@ function submitEventHandler(e) {
                 && document.querySelectorAll(`input[name=${el.name}]:checked`).length == 0
             )) {
 
-            handleEmptyInput(el)
+            if (!handleEmptyInput(el)) {
+                return false
+            }
 
         }
 
-        else {
-            console.log("vallejo", el.type, el.value)
-        }
     }
 
-    return
 
+    return true
 
-    alert("here again")
+}
+
+function createReadOnlyFormForDownload() {
 
     const formData = new FormData(form);
     script_str = ""
@@ -243,19 +226,45 @@ function submitEventHandler(e) {
     a.download = 'havad' + formData.get('personalNumber') + '.html';
     a.click();
     URL.revokeObjectURL(a.href);
+}
+
+function validateCommanderPhonesNotSame(){
+
+    const formData = new FormData(form);
+
+    if( formData.get('commanderPhone')  == formData.get('commander2Phone')  ) {
+        const popUpWrapper = genPopUp("מהמפקד העקיף", "להיות איש קשר שונה", "על המפקד הישיר");
+        document.getElementById("commander2Phone-wrapper").appendChild(popUpWrapper)
+
+        const eListener = () => {
+            popUpWrapper.remove()
+        }
+
+        document.getElementById("commander2Phone-wrapper").addEventListener("click", eListener)
+        document.getElementById("commander2Phone").addEventListener("input", eListener)
+        document.getElementById("commander2Phone").addEventListener("change", eListener)
+        form.addEventListener("submit", eListener)
+        
+        return false
+    }
+
+    return true
 
 }
 
-// document.addEventListener("DOMContentLoaded", () => {
 
-//     const form = document.getElementById("feedback-form");
-//     if (form) {
-//       form.addEventListener("submit", submitEventHandler);
-// } else {
-//   console.error("No element with ID 'feedback-form' found.");
-// }
-//   });
+// שליחת הטופס במייל
+function submitEventHandler(e) {
 
+    e.preventDefault() //Ignore submit default
+
+
+    if( !handleEmptyInputs ||  !validateCommanderPhonesNotSame() ){
+        return false
+    }
+
+    createReadOnlyFormForDownload() //Create the form for download
+}
 
 
 const form = document.getElementById("feedback-form");
