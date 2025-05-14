e3ca7fed0f567ba2472c1443314c082aeace48b6
Ruz <ruz@example.com>
2025-05-14 21:38:30 +0300

    split into routes

--- to_do_list_proj/app.py
+++ to_do_list_proj/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import HTTPException, status, Depends, APIRouter
 from fastapi.params import Body
 from sqlalchemy import text
 
@@ -8,19 +8,17 @@ from settings import settings
 
 from engine_and_session import get_session, engine
 from base_for_tables import Base
-from basemodels import NewItemRequest, getAllItemsResponse, Creds, LoginInput, LoginReturn, SignUpReturn
-from validate_oauth_creds import validate_login_input
-from handle_fastapi_auth import decode_jwt_token, ecncrypt_jwt_token, pwd_context, oauth2_scheme
-
+from basemodels import NewItemRequest, getAllItemsResponse
+from handle_fastapi_auth import decode_jwt_token, oauth2_scheme
 from application_instance import app
 
 # Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
-    
+
 
 @app.get("/")
-def f():
-    return {"message": "hi"}
+def g():
+    return {"message": "blah"}  
 
 @app.post("/old", status_code=status.HTTP_201_CREATED)
 def old_new_item( payload : dict = Body(...), s : Session = Depends(get_session) ):
@@ -59,53 +57,9 @@ def new_item( item : NewItemRequest , s : Session = Depends(get_session), jwt_to
     
     return {"message" : "created!"}
 
-def check_username_not_exist(username : str, s : Session):
-    return s.execute(text("select * from users where username = :username"), {"username" : username } ).fetchone()
-
-@app.post("/signup", status_code=status.HTTP_201_CREATED, response_model=SignUpReturn )
-def post_sign_up( creds : Creds,  s : Session = Depends(get_session)  ):
-    
-    
-    if check_username_not_exist(creds.username , s):
-        raise HTTPException( status_code=status.HTTP_409_CONFLICT, detail="username already exists" )
-    
-    s.execute( text("insert into users(username, password) values(:username , :password) ") , { "username" : creds.username, "password" : pwd_context.hash( creds.password ) }  )
-    s.commit()
-    
-    return { "username" : creds.username }
-
-
-@app.post("/login", response_model=LoginReturn)
-def login(creds : LoginInput = Depends(validate_login_input),  s : Session = Depends(get_session) ):
-    
-    res = s.execute(text("select * from users where username = :username"), creds.__dict__ ).mappings().fetchone()
-    
-    
-    if res and pwd_context.verify( creds.password , res['password'] ) :
-        
-        # token = jwt.encode( { "id" : res['id'] , "exp" : ( datetime.now(timezone.utc) + timedelta(minutes=100) )  },
-        #           settings.jwt_secret
-        #           ,algorithm="HS256" ) 
-        
-        token = ecncrypt_jwt_token( { "id" : res['id'] }, settings.jwt_secret, "HS256" )
-        
-        return {"access_token" : token , "token_type" : "bearer" }
-    
-    
-    raise HTTPException( status_code=status.HTTP_401_UNAUTHORIZED )
-
-
-       
-
-
-@app.get("/protected_test")
-def ppp( jwt_token : str = Depends(oauth2_scheme) ) :
-    
-    decode_jwt_token(jwt_token, settings.jwt_secret, "HS256" )
-    
-    return {"data" : "protected"}
-    
-    
 
 
 
+
+
+    
--- to_do_list_proj/application_instance.py
+++ to_do_list_proj/application_instance.py
@@ -1,12 +1,16 @@
 from starlette.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
+from router_auth import router_auth
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[],
-    allow_headers=[]
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
     
+    
+app.include_router(router_auth)
