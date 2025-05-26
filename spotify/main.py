40bec2ffdea7b5dda3a05c3ff8a81dcc7b92b079
Ruz <ruz@example.com>
2025-05-27 01:08:24 +0300

    all seems to be working :) fixed the playwright for scraping spotify to soemthing more accurate and specific what we need rather than relying on length

--- spotify/main.py
+++ spotify/main.py
@@ -1,50 +1,50 @@
 from playwright.sync_api import sync_playwright
-import time
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    context = browser.new_context(user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
-                                #   ,viewport={"width": 1200, "height": 100}  # set viewport size
-                                  )
-    page = context.new_page()
-    page.goto("https://youtube.com")
+def names_to_link(names):
     
+    res = []
     
-    searches = ["let her go passanger audio" , "one more time daft punk audio" ]
-    
-    prev_href = ""
-    
-    for search in searches:
-    
-        page.fill("input[name='search_query']", search )
-        # page.click("button[title='search']", timeout=1000000)
-        # page.click(".ytSearchboxComponentSearchButton", timeout=100000)
-        # page.click("yt-searchbox[role='search'] button")
-        
-        s = "xpath=(//yt-searchbox[@role='search']//button)[2]"
-        page.wait_for_selector(s)
-        page.query_selector(s).click()
-        
-        s2 = "ytd-video-renderer a#thumbnail"
-        # page.wait_for_timeout(10000)
-        
-        while True: 
-            page.wait_for_selector(s2)
-            if page.query_selector(s2).get_attribute('href') != prev_href:
-                break
-            page.wait_for_timeout(1000)
-        
-        prev_href = page.query_selector(s2).get_attribute('href')
-        print( "https://youtube.com" + prev_href )
+    searches = map( lambda p : p[0] + " " + p[1] + " " + "audio" , names)
+    print(searches)
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context(user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+                                    #   ,viewport={"width": 1200, "height": 100}  # set viewport size
+                                    )
+        page = context.new_page()
+        page.goto("https://youtube.com")
                 
-
+        prev_href = ""
         
-    page.wait_for_timeout(1000000)
-    
-    
-    browser.close()
-    
-    
-    
-    
-# document.querySelectorAll("[data-testid='tracklist-row']")
+        for search in searches:
+        
+            page.fill("input[name='search_query']", search )
+            # page.click("button[title='search']", timeout=1000000)
+            # page.click(".ytSearchboxComponentSearchButton", timeout=100000)
+            # page.click("yt-searchbox[role='search'] button")
+            
+            s = "xpath=(//yt-searchbox[@role='search']//button)[2]"
+            page.wait_for_selector(s)
+            page.query_selector(s).click()
+            
+            s2 = "ytd-video-renderer a#thumbnail"
+            # page.wait_for_timeout(10000)
+            
+            while True: 
+                page.wait_for_selector(s2)
+                if page.query_selector(s2).get_attribute('href') != prev_href:
+                    break
+                page.wait_for_timeout(1000)
+            
+            prev_href = page.query_selector(s2).get_attribute('href')
+            res.append( ("https://youtube.com" + prev_href, search) )
+            print( "https://youtube.com" + prev_href )
+                    
+        browser.close()
+        
+        
+    return res
+        
+    # document.querySelectorAll("[data-testid='tracklist-row']")
+
--- spotify/search_to_download.py
+++ spotify/search_to_download.py
@@ -1,51 +1,47 @@
 from playwright.sync_api import sync_playwright
+from scrape_from_spotify import scrape_from_spotify
+from main import names_to_link
 import time
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    context = browser.new_context(user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
-                                  ,accept_downloads = True
-                                #   ,viewport={"width": 1200, "height": 100}  # set viewport size
-                                  )
-    page = context.new_page()
-    page.goto("https://ytmp3.la/")
+def search_to_download(songs):
     
-    
-    songs = [ ("https://www.youtube.com/watch?v=VG3JsmOmDqw&pp=ygUsY2FudCBob2xkIHVzIG1hY2tlbG1vcmUgYW5kIHJ5YW4gbGV3aXMgYXVkaW8%3D" , "booby") ,
-              ("https://www.youtube.com/watch?v=MV_3Dpw-BRY&pp=ygUSbmlnaHRjYWxsIGthdmluc2t5" , "kavinsky") ,
-              ("https://www.youtube.com/watch?v=-DSVDcw6iW8&pp=ygULYSByZWFsIGhlcm8%3D", "realhero")
-             ]
-    
-    for song in songs: 
-    
-        page.locator("input#v").fill( song[0] )            
-        page.locator("button[type='submit']").click()        
-        page.wait_for_function(" document.querySelectorAll('button').length == 3 ") # make sure we have 3 buttons before we select based on button order
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context(user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+                                    ,accept_downloads = True
+                                    #   ,viewport={"width": 1200, "height": 100}  # set viewport size
+                                    )
+        page = context.new_page()
+        page.goto("https://ytmp3.la/")
         
         
-        page.evaluate("""
-    () => {
-        window.open = function(url, target, features) {
-            console.log('hi');
-        };
-    }
-    """)
+        for song in songs: 
         
-        with page.expect_download() as download_info:
-            page.query_selector("button").click()
-        print("outside:)")
-        print( download_info.value, download_info.value.save_as(f"results/{song[1]}.mp3") )
+            page.locator("input#v").fill( song[0] )    
+            page.locator("button[type='submit']").click()   
+            
+            page.wait_for_function(" document.querySelectorAll('button').length == 3 ", timeout=80000) # make sure we have 3 buttons before we select based on button order
+            
+            page.evaluate("""
+        () => {
+            window.open = function(url, target, features) {
+                console.log('hi');
+            };
+        }
+        """)
+            
+            with page.expect_download() as download_info:
+                page.query_selector("button").click()
+            print("outside:)")
+            print( download_info.value, download_info.value.save_as(f"results/{song[1]}.mp3") )
+            
+            
+            page.get_by_text("Next").click()
+                    
+        browser.close()
         
-        
-        page.get_by_text("Next").click()
-        
-    page.wait_for_timeout(1000000)
-    
-    browser.close()
-    
-    
-    
-    
-    
-    
-# document.querySelectorAll("[data-testid='tracklist-row']")
+    # document.querySelectorAll("[data-testid='tracklist-row']")
+
+search_to_download( names_to_link(
+scrape_from_spotify()
+    ) )
